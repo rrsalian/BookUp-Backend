793b3ae7949860b5b0591f44ddfa412c08b6f115
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-06-16 16:23:32 -0400

    Added Get users:id

--- functions/src/routes/userRoutes.ts
+++ functions/src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { getClient } from "../db";
 import { User } from "../models/User";
+import { ObjectId } from "mongodb";
 
 export const userRouter = express.Router();
 
@@ -19,6 +20,21 @@ userRouter.get('/users', async(req,res) => {
     }
 }
 )
+
+userRouter.get('/users/:id', async(req, res) => {
+  try {
+    const _id = new ObjectId(req.params.id); 
+    const client = await getClient();
+    const result = await client.db()
+          .collection<User>('users').findOne({ _id : _id }); 
+    console.log(result);
+    res.json(result);
+
+  } catch (err) {
+      console.error("ERROR", err);
+      res.status(500).json({message: 'Internal Server Error'});
+  }
+});
 
 userRouter.post('/users', async(req, res) => {
     try {  
